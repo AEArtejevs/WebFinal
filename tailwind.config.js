f8fb4ef3bb066852bc23b6823319bbb01f845729
AndrisEdgarsArtejevs <andrisedgars.artejevs@va.lv>
2025-03-22 20:21:50 +0200

    migrations

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        "./node_modules/flowbite/**/*.js",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [
+        forms,
+        require('@tailwindcss/forms'),
+        require('flowbite/plugin'),
+    ],
+};
